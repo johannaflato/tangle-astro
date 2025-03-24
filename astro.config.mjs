52200308af2eb18eb49b64c08c72ea56687203cd
johannaflato <jcflato@gmail.com>
2025-03-24 14:13:39 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://johannaflato.github.io',
+	// base: 'tangle-astro',
 	vite: {
 		plugins: [tailwindcss()]
 	}
