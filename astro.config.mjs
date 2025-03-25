7326371c4e5fc02b1dc36e4f04880b76bf0eef62
johannaflato <jcflato@gmail.com>
2025-03-25 16:37:23 -0400

    astro.config.mjs & mdx.astro simplified
    - removing custom mdx plugins/integrations that were impeding runtime, and other MDXProps adjustments
    - issue: still not rendering correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-import { recmaPlugins } from './src/integrations/mdx/recma.mjs';
-import { rehypePlugins } from './src/integrations/mdx/rehype.mjs';
-import { remarkPlugins } from './src/integrations/mdx/remark.mjs';
+// import { recmaPlugins } from './src/integrations/mdx/recma.mjs';
+// import { rehypePlugins } from './src/integrations/mdx/rehype.mjs';
+// import { remarkPlugins } from './src/integrations/mdx/remark.mjs';
 
 export default defineConfig({
   site: 'https://johannaflato.github.io',
@@ -15,10 +15,7 @@ export default defineConfig({
   integrations: [
     mdx({
       syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'css-variables' },
-      remarkPlugins: remarkPlugins,
-      rehypePlugins: rehypePlugins,
-      recmaPlugins: recmaPlugins
+      shikiConfig: { theme: 'css-variables' }
     }),
     react()
   ]
